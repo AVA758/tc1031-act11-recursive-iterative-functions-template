228f3dc2079dbd38728efb898f0b374a176c01d3
AVA758 <89211436+AVA758@users.noreply.github.com>
2022-08-26 17:15:06 -0500

    Add files via upload

---
+++ activity.h.h
@@ -0,0 +1,51 @@
+// =================================================================
+//
+// File: activity.h
+// Author: Alain Vicencio Arizabalo
+// Date: 26/08/2022
+//
+// =================================================================
+#ifndef ACTIVITY_H
+#define ACTIVITY_H
+
+// =================================================================
+// sumaIterativa. Calculate the sum from 1 to n with an iterative
+// method.
+//
+// @param n	The maximum limit of the sum.
+// @return	The result of the addition from 1 to n.
+// @Complexity	O(n)
+// =================================================================
+unsigned int sumaIterativa(unsigned int n) {
+	int res=0;
+  for(int i=1; i<=n; i++)
+    res= res+i;
+  return res;
+}
+
+// =================================================================
+// sumaRecursiva. Calculate the sum from 1 to n with an recursive
+// method.
+//
+// @param n	The maximum limit of the sum.
+// @return	The result of the addition from 1 to n.
+// @Complexity	O(n)
+// =================================================================
+unsigned int sumaRecursiva(unsigned int n) {
+	if (n<=1) return 1;
+  return n+sumaRecursiva(n-1);
+}
+
+// =================================================================
+// sumaDirecta. Calculate the sum from 1 to n with a mathematical
+// method.
+//
+// @param n	The maximum limit of the sum.
+// @return	The result of the addition from 1 to n.
+// @Complexity	O(1)
+// =================================================================
+unsigned int sumaDirecta(unsigned int n) {
+	return n*(n+1)/2;
+}
+
+#endif /* ACTIVITY_H */
